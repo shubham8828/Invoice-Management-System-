c0b4d350b97c97f31c6645a9f0d64b2cb8f83bdc
Shubham Vishwakarma <143716883+shubham8828@users.noreply.github.com>
2024-09-19 02:14:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://invoice-managementsystem.vercel.app"],
+        origin: ["https://aims1.vercel.app/"],
         methods: ["POST", "GET","PUT","DELETE"],
         credentials: true
     }
